bc4e572b439975916b27b680af092a49f26d448f
Andre <andddre@bol.com.br>
2016-09-07 10:38:28 -0300

    Removido construtor e membro desc.

--- app/src/main/java/br/com/pandaria/Entity/TipoDeIngrediente.java
+++ app/src/main/java/br/com/pandaria/Entity/TipoDeIngrediente.java
@@ -1,17 +1,6 @@
 package br.com.pandaria.Entity;
 
 public enum TipoDeIngrediente {
-
-    INGREDIENTE("INGREDIENTE"),
-    EMBALAGEM("EMBALAGEM");
-
-    private String desc;
-
-    TipoDeIngrediente(String desc){
-        this.desc = desc;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
+    INGREDIENTE,
+    EMBALAGEM
 }
