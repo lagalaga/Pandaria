bdf305d4094ba24fbbdcf199a3969270da9c429a
Andre <andddre@bol.com.br>
2016-09-07 10:35:30 -0300

    Modificado método listarDespesasPorData (removido colunas na clausula query e adicionado null no local)

--- app/src/main/java/br/com/pandaria/Dao/DaoDespesa.java
+++ app/src/main/java/br/com/pandaria/Dao/DaoDespesa.java
@@ -69,11 +69,11 @@ public class DaoDespesa {
 
     }
 
-    public List<Despesa> listarDespesasPorDatA(SQLiteOpenHelper helper,Date dataInicial,Date dataFinal){
+    public List<Despesa> listarDespesasPorData(SQLiteOpenHelper helper,Date dataInicial,Date dataFinal){
 
         this.db = helper.getReadableDatabase();
         List<Despesa> despesas = new ArrayList<>();
-        Despesa despesa = new Despesa();
+        Despesa despesa;
         SimpleDateFormat formatterParaClasse = new SimpleDateFormat("dd/MM/yyyy");
         SimpleDateFormat formatterParaBd = new SimpleDateFormat("yyyy-MM-dd");
 
@@ -81,19 +81,12 @@ public class DaoDespesa {
         String inicio = formatterParaBd.format(dataFinal);
         String fim = formatterParaBd.format(dataFinal);
 
-
-        String[] colunas = {
-                DespesaContract.Despesa.NOME_COLUNA_DESCRICAO,
-                DespesaContract.Despesa.NOME_COLUNA_VALOR,
-                DespesaContract.Despesa.NOME_COLUNA_DATA
-        };
-
         String selection = DespesaContract.Despesa.NOME_COLUNA_DATA + " BETWEEN ? AND ? ";
         String[] selectionArgs = {inicio,fim};
 
         Cursor c = db.query(
                 DespesaContract.Despesa.NOME_TABELA,
-                colunas,
+                null,
                 selection,
                 selectionArgs,
                 null,
@@ -104,7 +97,7 @@ public class DaoDespesa {
         c.moveToFirst();
         while(c.moveToNext()){
 
-            //despesa = new Despesa();
+            despesa = new Despesa();
             despesa.setId(c.getInt(c.getColumnIndex(DespesaContract.Despesa._ID)));
             despesa.setDescricao(c.getString(c.getColumnIndex(DespesaContract.Despesa.NOME_COLUNA_DESCRICAO)));
             despesa.setValor(c.getFloat(c.getColumnIndex(DespesaContract.Despesa.NOME_COLUNA_VALOR)));
