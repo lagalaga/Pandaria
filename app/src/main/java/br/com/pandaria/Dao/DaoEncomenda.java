484b274a1768278d04be7c3b1d2af217e2634aa1
Andre <andddre@bol.com.br>
2016-09-09 17:09:18 -0300

    Criado parte do método listarEncomendas

--- app/src/main/java/br/com/pandaria/Dao/DaoEncomenda.java
+++ app/src/main/java/br/com/pandaria/Dao/DaoEncomenda.java
@@ -1,10 +1,18 @@
 package br.com.pandaria.Dao;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.text.Selection;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import br.com.pandaria.Entity.Encomenda;
+import br.com.pandaria.Entity.Venda;
 
 public class DaoEncomenda {
 
@@ -64,7 +72,53 @@ public class DaoEncomenda {
         values.put(EncomendaContract.Encomenda.NOME_COLUNA_DESCRICAO,encomenda.getDescricao());
         values.put(EncomendaContract.Encomenda.NOME_COLUNA_VALOR,encomenda.getValor());
 
-        return true;
+        String selection = EncomendaContract.Encomenda.NOME_COLUNA_FK_VENDA + " = ?";
+        String[] args = {Long.toString(encomenda.getId())};
+
+        daoVenda.atualizar(encomenda,helper);
+
+
+        if(db.update(EncomendaContract.Encomenda.NOME_TABELA,values,selection,args) > 0){
+            db.close();
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+    public List<Encomenda> listarEncomendas(SQLiteOpenHelper helper,Date dataInicial,Date dataFinal){
+
+        this.db = helper.getReadableDatabase();
+        List<Encomenda> encomendas = new ArrayList<>();
+        Encomenda encomenda;
+
+        SimpleDateFormat formatterParaClasse = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat formatterParaBd = new SimpleDateFormat("yyyy-MM-dd");
+
+        String inicio = formatterParaBd.format(dataInicial);
+        String fim = formatterParaBd.format(dataFinal);
+
+        // Join na tabela venda e encomenda
+        String query = "SELECT * FROM " +
+                VendaContract.Venda.NOME_TABELA + "JOIN" +
+                EncomendaContract.Encomenda.NOME_TABELA +
+                " ON " + VendaContract.Venda._ID + " = " +
+                EncomendaContract.Encomenda.NOME_COLUNA_FK_VENDA +
+                "WHERE " + VendaContract.Venda.NOME_COLUNA_DATA + " BETWEEN " + inicio + " AND " + fim ;
+
+        Cursor c = db.rawQuery(query,null);
+
+        c.moveToFirst();
+
+        while(c.moveToNext()){
+
+            encomenda = new Encomenda();
+            encomenda.setId(c.getLong(c.getColumnIndex(VendaContract.Venda._ID)));
+
+        }
+
+
 
     }
 
