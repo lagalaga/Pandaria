f1d44c3450ddee8ebfeb9213b6af170017cc5058
Andre <andddre@bol.com.br>
2016-09-12 14:26:04 -0300

    Criado View Ingrediente

---
+++ app/src/main/java/br/com/pandaria/View/InserirIngrediente.java
@@ -0,0 +1,15 @@
+package br.com.pandaria.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.silvasoa2.pandaria.R;
+
+public class InserirIngrediente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inserir_ingrediente);
+    }
+}
