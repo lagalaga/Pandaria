035cc6a4aea6ec91cc6b16e6be4c9744f5b145f8
Andre <andddre@bol.com.br>
2016-09-16 16:25:34 -0300

    Criado Intente para Editar Ingrediente

--- app/src/main/java/br/com/pandaria/View/ListarIngrediente.java
+++ app/src/main/java/br/com/pandaria/View/ListarIngrediente.java
@@ -3,6 +3,7 @@ package br.com.pandaria.View;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,11 +20,13 @@ import android.widget.Toast;
 import com.example.silvasoa2.pandaria.R;
 
 
+import java.io.Serializable;
 import java.util.List;
 
 import br.com.pandaria.Dao.PandariaDbHelper;
 import br.com.pandaria.Entity.Ingrediente;
 import br.com.pandaria.Business.b_Ingrediente;
+import br.com.pandaria.Entity.TipoDeIngrediente;
 
 public class ListarIngrediente extends AppCompatActivity {
 
@@ -32,17 +35,26 @@ public class ListarIngrediente extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_ingrediente);
 
-        final PandariaDbHelper dbHelper = new PandariaDbHelper(this);
         List<Ingrediente> ingredientes;
-        ingredientes = new b_Ingrediente().b_ListarIngredientes(0,dbHelper);
+        ingredientes = new b_Ingrediente().b_ListarIngredientes(0,this);
         final ListView listaIngre = (ListView) findViewById(R.id.list_ingredientes);
+
+
+        //somente para teste. Apagar depois
+        Ingrediente testeIngre = new Ingrediente();
+        testeIngre.setId(1);
+        testeIngre.setNome("andrezao");
+        testeIngre.setQtdDoPacote(1.2F);
+        testeIngre.setTipoDeIngrediente(TipoDeIngrediente.EMBALAGEM.toString());
+        ingredientes.add(testeIngre);
 
 
         if(ingredientes.isEmpty()){
             TextView txt = new TextView(this);
             txt.setText(R.string.string_lblListaIngreVazia);
             txt.setId(0);
-            txt.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+            txt.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT));
 
             RelativeLayout relative = (RelativeLayout) findViewById(R.id.view_list_ingredientes);
@@ -55,13 +67,18 @@ public class ListarIngrediente extends AppCompatActivity {
 
             ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,ingredientes);
             listaIngre.setAdapter(adapter);
-            final Toast teste = Toast.makeText(this,"teste",Toast.LENGTH_SHORT);
             listaIngre.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     Ingrediente ingredienteSelecionado = (Ingrediente) listaIngre.getItemAtPosition(position);
-                    teste.setText(ingredienteSelecionado.getNome());
-                    teste.show();
+                    Intent intent = new Intent(ListarIngrediente.this,EditarIngrediente.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("teste",(Serializable)ingredienteSelecionado);
+                    intent.putExtra("objeto",bundle);
+                    startActivity(intent);
+
+
+
                 }
             });
 
