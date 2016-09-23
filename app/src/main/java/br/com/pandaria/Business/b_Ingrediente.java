35b0f205f4c232080381aa8694f6d7c6e582a4c9
Andre <andddre@bol.com.br>
2016-09-23 13:31:06 -0300

    Metodo deletar checa se ingrediente é utilizado

--- app/src/main/java/br/com/pandaria/Business/b_Ingrediente.java
+++ app/src/main/java/br/com/pandaria/Business/b_Ingrediente.java
@@ -3,11 +3,16 @@ package br.com.pandaria.Business;
 import android.content.Context;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.view.View;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.pandaria.Dao.DaoIngrediente;
+import br.com.pandaria.Dao.DaoProduto;
+import br.com.pandaria.Dao.DaoProdutoIngrediente;
 import br.com.pandaria.Entity.Ingrediente;
+import br.com.pandaria.Entity.Produto;
 
 public class b_Ingrediente {
 
@@ -24,6 +29,24 @@ public class b_Ingrediente {
     }
 
     public boolean b_Deletar(long id,Context context) {
-        return new DaoIngrediente(context).deletar(id);
+
+        // Checa se não existem produtos com o ingrediente deletado
+        List<Produto> produtos, produtosComIngredientes;
+        List<Produto> listaProduto = new ArrayList<>();
+
+        produtos = new DaoProdutoIngrediente(context).listarProdutosComIngrediente(id);
+        if(!produtos.isEmpty()){
+            for(Produto produto: produtos){
+                produtosComIngredientes = new DaoProduto(context).listarProdutos(produto.getId());
+                produto = produtosComIngredientes.get(0);
+                listaProduto.add(produto);
+            }
+            Toast t = Toast.makeText(context,"Existem produtos com este ingrediente",Toast.LENGTH_SHORT);
+            return false;
+        } else {
+
+            return new DaoIngrediente(context).deletar(id);
+        }
+
     }
 }
