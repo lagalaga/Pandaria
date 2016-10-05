d52309007f3bf0882c1734d16bd7f8db164c32e2
Andre <andddre@bol.com.br>
2016-10-05 17:55:07 -0300

    Classe implementa callbacks do dialog QTD

--- app/src/main/java/br/com/pandaria/View/InserirProduto.java
+++ app/src/main/java/br/com/pandaria/View/InserirProduto.java
@@ -2,45 +2,54 @@ package br.com.pandaria.View;
 
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
-import android.widget.SimpleCursorTreeAdapter;
 import android.widget.TextView;
 
 import com.example.silvasoa2.pandaria.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import br.com.pandaria.Business.b_Estoque;
 import br.com.pandaria.Dao.DaoIngrediente;
+import br.com.pandaria.Entity.Estoque;
 import br.com.pandaria.Entity.Ingrediente;
 import br.com.pandaria.Entity.Produto;
 import br.com.pandaria.Utility.AdapterListIngredienteProduto;
+import br.com.pandaria.Utility.DialogListIngredienteProduto;
 
-public class InserirProduto extends AppCompatActivity {
+public class InserirProduto extends AppCompatActivity implements DialogListIngredienteProduto.NoticeDialogListener{
 
-    AdapterListIngredienteProduto adapterIngreProd = null;
+    private b_Estoque bEstoque;
+    private AdapterListIngredienteProduto adapterIngreProd = null;
+    private HashMap<Ingrediente,Float> escolhidos;
+    private List<Ingrediente> ingredientes;
+    private float custo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inserir_produto);
 
+        //Instancia os membros
+        escolhidos = new HashMap<>();
+        bEstoque = new b_Estoque();
+
         //Pegar componentes da tela
         final EditText txtNome = (EditText) findViewById(R.id.txtNomeProduto);
         final EditText txtPreco = (EditText) findViewById(R.id.txtPrecoProduto);
-        final TextView lblCusto = (TextView) findViewById(R.id.lblCustoCalculado);
         final Button btnIncluir = (Button) findViewById(R.id.btnIncluir);
-        final ListView lstEmbalagens = (ListView) findViewById(R.id.lstEmbalagemTelaProdutos);
-        final ExpandableListView lstIngrediente = (ExpandableListView) findViewById(R.id.explstIngrediente);
+
 
         //Listar todos os ingredientes
-       List<Ingrediente> ingredientes = new DaoIngrediente(this).listarIngredientes(0);
+    ingredientes = new DaoIngrediente(this).listarIngredientes(0);
 
         //TODO testar as lists APAGAR ISSO DEPOIS
         Ingrediente ingreTeste;
@@ -55,6 +64,60 @@ public class InserirProduto extends AppCompatActivity {
             }
             ingredientes.add(ingreTeste);
         }
+
+        preencheLista(ingredientes);
+
+
+
+        btnIncluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Produto produto = new Produto();
+                produto.setNome(txtNome.getText().toString());
+                produto.setPrecoDeVenda(Float.parseFloat(txtPreco.getText().toString()));
+
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onDialogPositiveClick(Float qtdUsada, Ingrediente ingrediente) {
+
+        escolhidos.put(ingrediente,qtdUsada);
+        ingrediente.setEscolhido(true);
+
+        //Pega todos as entradas do ingrediente
+        List<Estoque> estoques = bEstoque.b_BuscarEstoquestPorIngrediente(ingrediente.getId(),this);
+
+        //Calcula o Custo
+        custo = custo + br.com.pandaria.Utility.Custo.custoPorIngrediente(estoques.get(0).getPrecoUnit(),ingrediente.getQtdDoPacote(),qtdUsada);
+
+        //Mostra o custo atual
+        final TextView lblCusto = (TextView) findViewById(R.id.lblCustoCalculado);
+        lblCusto.setText(Float.toString(custo));
+
+        //Atualiza lista
+        preencheLista(ingredientes);
+
+
+
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        dialog.dismiss();
+
+    }
+
+    private void preencheLista(List<Ingrediente> ingredientes){
+
+        final ListView lstEmbalagens = (ListView) findViewById(R.id.lstEmbalagemTelaProdutos);
+        final ExpandableListView lstIngrediente = (ExpandableListView) findViewById(R.id.explstIngrediente);
+
 
         //Separa ingredientes entre embalagem e ingrediente
         List<Ingrediente> embalagens = new ArrayList<>();
@@ -80,16 +143,7 @@ public class InserirProduto extends AppCompatActivity {
         lstEmbalagens.setAdapter(adapterIngreProd);
 
 
-        btnIncluir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Produto produto = new Produto();
-                produto.setNome(txtNome.getText().toString());
-                produto.setPrecoDeVenda(Float.parseFloat(txtPreco.getText().toString()));
 
-
-            }
-        });
 
     }
 }
